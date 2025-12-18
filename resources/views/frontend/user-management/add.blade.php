9f5702e2b7a33c4abd8c11bce7b47574ee290cf3
Parmar034 <bhavesh@karmaasource.com>
2025-12-18 10:31:06 +0530

    user management add file chnages

--- resources/views/frontend/user-management/add.blade.php
+++ resources/views/frontend/user-management/add.blade.php
@@ -53,7 +53,7 @@
                                             value="{{ $member->id ?? old('user_id') }}">
 
 
-                                        @if(Auth::user()->role == 'Super Admin')    
+                                        @if(Auth::user()->role != 'Admin')    
                                         <div class="row">    
                                             <div class="col-xxl-4 col-xl-4 col-lg-6 col-md-6 col-sm-6 col-xs-12">
                                                 <label class="form-label">Member</label>
